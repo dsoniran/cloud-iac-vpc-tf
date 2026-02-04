5df448af870ccaa5d81de254f3a89a12823ff4da
Dáre <78746521+dsoniran@users.noreply.github.com>
2026-02-04 14:31:36 +0000

    Added terraform/

---
+++ terraform/create-ec2/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "eu-west-1"
+  #   profile = "default"
+  #   shared_config_files = ["~/.aws/config"]
+  #   shared_credentials_files = ["~/.aws/credentials"]
+}
+
+# Which resource do we want to create?
+resource "aws_instance" "test_instance_1" {
+
+  # AMI_ID
+  ami = "ami-03446a3af42c5e74e"
+
+  # Instance Type
+  instance_type = "t3.micro"
+
+  # add public IP
+  associate_public_ip_address = true
+
+  #   # Key-Pair for SSH access
+  #   key_name = "se-dare-key-pair"
+
+  # Tags // Name of the instance
+  tags = {
+    Name = "se-dare-tf-instance"
+  }
+}
