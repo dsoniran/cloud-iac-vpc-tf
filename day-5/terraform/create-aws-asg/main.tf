3d627650d1e2de70e530b4ab581f1541dacc2b18
Dáre <78746521+dsoniran@users.noreply.github.com>
2026-02-06 23:16:17 +0000

    Updated main.tf

--- day-5/terraform/create-aws-asg/main.tf
+++ day-5/terraform/create-aws-asg/main.tf
@@ -36,96 +36,6 @@ resource "aws_ami_from_instance" "ami_from_instance" {
   depends_on         = [aws_instance.ec2_app_instance_for_image]
 }
 
-# resource "aws_imagebuilder_distribution_configuration" "app_image_distribution_configuration" {
-#   name = "se-dare-tf-app-image-distribution-configuration"
-
-#   distribution {
-#     ami_distribution_configuration {
-#       ami_tags = {
-#         # CostCenter = "IT"
-#       }
-
-#       name = "se-dare-tf-app-image-{{ imagebuilder:buildDate }}"
-
-#       launch_permission {
-#         # user_ids = ["123456789012"]
-#       }
-#     }
-
-#     # launch_template_configuration {
-#       # launch_template_id = "lt-0aaa1bcde2ff3456"
-#     # }
-
-#     region = var.region
-#   }
-# }
-
-# resource "aws_imagebuilder_component" "app_image_receipe_component" {
-#   # data = yamlencode({
-#   #   phases = [{
-#   #     name = "build"
-#   #     steps = [{
-#   #       action = "ExecuteBash"
-#   #       inputs = {
-#   #         commands = ["echo 'hello world'"]
-#   #       }
-#   #       name      = "example"
-#   #       onFailure = "Continue"
-#   #     }]
-#   #   }]
-#   #   schemaVersion = 1.0
-#   # })
-#   name     = "se-dare-tf-app-image-recipe-component"
-#   platform = "Linux"
-#   version  = "1.0.0"
-# }
-
-# resource "aws_imagebuilder_image_recipe" "app_image_recipe" {
-#   name = "se-dare-tf-app-image-recipe"
-#   parent_image = var.app_ami
-#   version = "1.0.0"
-
-#   block_device_mapping {
-#     device_name = "/dev/sda1"
-
-#     ebs {
-#       delete_on_termination = true
-#       volume_size           = 8
-#       volume_type           = "gp3"
-#     }
-#   }
-
-#   component {
-#     component_arn = aws_imagebuilder_component.app_image_receipe_component.arn
-#   }
-
-# }
-
-# resource "aws_imagebuilder_infrastructure_configuration" "app_image_infrastructure_configuration" {
-#   name = "se-dare-tf-app-image-infrastructure-configuration"
-#   description = "Infrastructure configuration for application image"
-#   instance_profile_name = "se-dare-tf-instance-profile"
-#   instance_types = [var.instance_type]
-#   key_pair = var.key_pair
-
-#   # Network settings
-#   security_group_ids = [aws_security_group.app_sg.id]
-#   sns_topic_arn = "arn:aws:sns:eu-west-1:123456789012:my-sns-topic"
-#   subnet_id = aws_subnet.public_subnet.id
-#   terminate_instance_on_failure = true
-
-#   tags = {
-#     Name = "se-dare-tf-app-image-distribution-configuration"
-#   }
-
-# }
-
-# resource "aws_imagebuilder_image" "app_image" {
-#   distribution_configuration_arn = aws_imagebuilder_distribution_configuration.app_image_distribution_configuration.arn
-#   image_recipe_arn = aws_imagebuilder_image_recipe.app_image_recipe.arn
-#   infrastructure_configuration_arn = aws_imagebuilder_infrastructure_configuration.app_image_infrastructure_configuration.arn
-# }
-
 resource "aws_launch_template" "launch_template" {
   name = "se-dare-tf-launch-template"
   description = "Launch template with terraform"
@@ -142,31 +52,6 @@ resource "aws_launch_template" "launch_template" {
     subnet_id = aws_subnet.public_subnet.id
     associate_public_ip_address = var.app_allow_public_ip_address
   }
-
-  # user_data = filebase64("${path.module}/user_data.sh")
-
-  # user_data = base64encode(<<-EOF
-  #     #!/bin/bash
-
-  #     # enter directory with application script
-  #     cd nodejs2-sparta-test-app-2025/app
-
-  #     # install npm
-  #     sudo npm install --yes
-
-  #     # export database private ip address
-  #     export DB_HOST="$(terraform output -raw db_host)"
-
-  #     # kill any active operations
-  #     pm2 kill
-
-  #     # seed the data
-  #     node seeds/seed.js
-
-  #     # start app
-  #     pm2 start app.js
-  #   EOF
-    # )
 
     user_data = base64encode(
       templatefile("${path.module}/user_data.sh", {
